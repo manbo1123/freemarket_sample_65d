711ed35ba1139a99dc79b8c768b8ea7bc18c6869
manbo1123 <manbobinaccomplish@yahoo.co.jp>
2020-03-09 23:23:48 +0900

    change to pluck-method

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,10 +7,7 @@ class ItemsController < ApplicationController
     @item = Item.new
 
     #親カテゴリー
-    @category_parent_array = ["選択してください"]   #親カテゴリーの初期値(配列)
-    Category.where(ancestry: nil).each do |parent|  #テーブルから、親のみ取り出して、配列化
-      @category_parent_array << parent.name
-    end
+    @category_parent_array = Category.where(ancestry: nil).pluck(:name)
   end
   
   def create

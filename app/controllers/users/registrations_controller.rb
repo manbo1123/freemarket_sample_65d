e1286eb5f2651cbc43dbc6163ca12fddbaaa0d51
kz-oita <taoi.kizuka@gmail.com>
2020-03-22 18:24:18 +0900

    ridirect_toをprefixに修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -31,36 +31,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
     if @user.save
       sign_in(:user, @user)
-      redirect_to controller: '/cards', action: 'new'
+      redirect_to new_card_path
     else
       render :new_sending_destination
     end
   end
-
-
-  # GET /resource/edit
-  # def edit
-  #   super
-  # end
-
-  # PUT /resource
-  # def update
-  #   super
-  # end
-
-  # DELETE /resource
-  # def destroy
-  #   super
-  # end
-
-  # GET /resource/cancel
-  # Forces the session data which is usually expired after sign
-  # in to be expired now. This is useful if the user wants to
-  # cancel oauth signing in/up in the middle of the process,
-  # removing all OAuth session data.
-  # def cancel
-  #   super
-  # end
 
   protected
   def configure_sign_up_params
@@ -71,18 +46,4 @@ class Users::RegistrationsController < Devise::RegistrationsController
     params.require(:sending_destination).permit(:destination_family_name, :destination_first_name, :destination_family_name_kana, :destination_first_name_kana, :post_code, :prefecture_id, :city, :house_number, :building_name, :phone_number)
   end
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
-
-  # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
-
-  # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
 end

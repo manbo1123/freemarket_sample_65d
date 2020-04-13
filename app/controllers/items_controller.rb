de8e722e3eb54c4fa97565997a3b85903b166a45
kz-oita <taoi.kizuka@gmail.com>
2020-04-13 17:30:29 +0900

    befor_actionの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_category, only: [:new, :edit, :create, :update, :destroy]
   before_action :set_item, only: [:show, :edit, :update, :destroy, :purchase, :buy]
   before_action :set_card, only: [:purchase, :buy]

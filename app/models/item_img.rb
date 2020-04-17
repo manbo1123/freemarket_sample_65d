e60307a067bedf0c1127504f7846402e16879ac8
ponpon2 <gkqr84@icloud.com>
2020-04-17 19:13:55 +0900

    修正

--- app/models/item_img.rb
+++ app/models/item_img.rb
@@ -1,6 +1,6 @@
 class ItemImg < ApplicationRecord
   mount_uploader :src, ImageUploader
-  belongs_to :item, optional: true, dependent: :destroy
+  belongs_to :item, optional: true
 
   validates :src, presence: true
 end

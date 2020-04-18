97e264767aa96f566812d023a060dfd8b2f0f7e6
masashi0202 <1993.masashi@gmail.com>
2020-04-18 15:07:54 +0900

    [マイページ]出品・購入した商品タブ編集

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -1,7 +1,5 @@
 class Api::ItemsController < ApplicationController
   def index
-    # ajaxで送られてくる最後のメッセージのid番号を変数に代入
-    # category_id = params[:id]
     @children = Category.find(params[:id]).children
   end
 end

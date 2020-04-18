9f98ac526da4fd03e4d33d4c4cdf4d143aee802d
kz-oita <taoi.kizuka@gmail.com>
2020-04-18 15:20:42 +0900

    購入機能の単体テスト

--- spec/factories/cards.rb
+++ spec/factories/cards.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :card do
-    
+    customer_id {"test_customer_id"}
+    card_id     {"test_card_id"}
+    user_id     {1}
   end
 end
--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
-RSpec.describe Card, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Card do
+  describe '#create' do
+    context 'can save' do
+      it "customer_id, card_id, user_id,が存在すれば登録できる" do
+        expect(build(:card)).to be_valid
+      end
+    end
+
+    context 'can not save' do
+      it "customer_idが存在しないと登録できない" do
+        card = build(:card, customer_id: nil)
+        card.valid?
+        expect(card.errors[:customer_id]).to include("を入力してください")
+      end
+      it "card_idが存在しないと登録できない" do
+        card = build(:card, card_id: nil)
+        card.valid?
+        expect(card.errors[:card_id]).to include("を入力してください")
+      end
+      it "user_idが存在しないと登録できない" do
+        card = build(:card, user_id: nil)
+        card.valid?
+        expect(card.errors[:user_id]).to include("を入力してください")
+      end
+    end
+  end
 end

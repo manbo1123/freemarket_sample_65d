dcf026f65c76c794b6b40d7a14ec54d4acd8d49e
08kentaro <michaelthearchangel5075@yahoo.co.jp>
2020-05-02 05:54:28 +0900

    いいね！一覧の表示

--- app/controllers/mypage/favorites_controller.rb
+++ app/controllers/mypage/favorites_controller.rb
@@ -2,30 +2,10 @@ class Mypage::FavoritesController < ApplicationController
   before_action :authenticate_user
 
   def index
-    if request.fullpath == "/mypage/favorite/selling"
-      @myfavorites = Favorite.where(user_id: current_user.id)
-      @favoritesitem_id = @myfavorites.pluck(:item_id)
-      @favorite_list = Item.find(@favoritesitem_id).where(buyer_id: nil)
-    elsif request.fullpath == "/mypage/favorite/closed"
-      @myfavorites = Favorite.where(user_id: current_user.id)
-      @favoritesitem_id = @myfavorites.pluck(:item_id)
-      @favorite_list = Item.find(@favoritesitem_id).where.not(buyer_id: nil)
-    end
-  end
+    @myfavorites = Favorite.where(user_id: current_user.id)
+    @favoritesitem_id = @myfavorites.pluck(:item_id)
+    @favorite_list = Item.find(@favoritesitem_id)
 
-  def set_favorite
-    # if params[:id] == "0"
-    #   favoriteIds = Item.where(seller_id: current_user.id).where(trading_status: 0).where(buyer_id: nil).pluck(:id)
-    # elsif params[:id] == "1"
-    #   exhibitionIds = Item.where(seller_id: current_user.id).where(trading_status: [0, 1]).where.not(buyer_id: nil).pluck(:id)
-    # else
-    #   exhibitionIds = Item.where(seller_id: current_user.id).where(trading_status: 2).where.not(buyer_id: nil).pluck(:id)
-    # end
-    @favorites = []
-      @myfavorites = Favorite.where(user_id: current_user.id)
-      @favoritesitem_id = @myfavorites.pluck(:item_id)
-      @favorite_list = Item.find(@favoritesitem_id)
-      @favorites << favorites
+    @fa = Favorite.where(item_id: @favoritesitem_id)
   end
-
 end
